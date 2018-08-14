1cb25e2ed47e8ac0731fd48194d14fdf01a6aa33
border <border.gong@gmail.com>
2018-08-14 21:10:00 +0800

    add dummyhead

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -23,11 +23,11 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head;
+    private Node dummyHead;
     private int size;
 
     public LinkedList(){
-        head = null;
+        dummyHead = new Node(null, null);
         size = 0;
     }
 
@@ -39,30 +39,22 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    public void addFirst(E e){
-//        Node node = new Node(e);
-//        node.next = head;
-//        head = node;
-
-        head = new Node(e, head);
-        size ++;
-    }
-
     public void add(int index, E e){
         if(index < 0 || index > size)
             throw new IllegalArgumentException("Add failed, Illegal Index.");
-        if(index == 0)
-            addFirst(e);
-        else {
-            Node prev = head;
-            for (int i = 0; i < index - 1; i++)
-                prev = prev.next;
 
-            Node node = new Node(e);
-            node.next = prev.next;
-            prev.next = node;
-            size ++;
-        }
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++)
+            prev = prev.next;
+
+        Node node = new Node(e);
+        node.next = prev.next;
+        prev.next = node;
+        size ++;
+    }
+
+    public void addFirst(E e){
+        add(0, e);
     }
 
     public void addLast(E e){
