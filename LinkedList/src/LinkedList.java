da354943c052ad48cdf18650bc5002c1da5f0e2e
border <border.gong@gmail.com>
2018-08-15 21:52:43 +0800

    LinkedList
    get方法 contains方法 toString方法

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -31,14 +31,27 @@ public class LinkedList<E> {
         size = 0;
     }
 
+    /**
+     * 获取容量
+     * @return
+     */
     public int getSize(){
         return size;
     }
 
+    /**
+     * 是否为空
+     * @return
+     */
     public boolean isEmpty(){
         return size == 0;
     }
 
+    /**
+     * 添加元素
+     * @param index
+     * @param e
+     */
     public void add(int index, E e){
         if(index < 0 || index > size)
             throw new IllegalArgumentException("Add failed, Illegal Index.");
@@ -53,11 +66,95 @@ public class LinkedList<E> {
         size ++;
     }
 
+    /**
+     * 添加第一个元素
+     * @param e
+     */
     public void addFirst(E e){
         add(0, e);
     }
 
+    /**
+     * 添加最后一个元素
+     * @param e
+     */
     public void addLast(E e){
         add(size, e);
+    }
+
+    /**
+     * 获取索引位置的元素
+     * @param index
+     * @return
+     */
+    public E get(int index){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("index out of bound!");
+
+        Node cur = dummyHead.next;
+        for(int i = 0; i < index; i ++){
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    /**
+     * 获取第一个元素
+     * @return
+     */
+    public E getFirst(){
+        return get(0);
+    }
+
+    /**
+     * 获取最后一个元素
+     * @return
+     */
+    public E getLast(){
+        return get(size - 1);
+    }
+
+    /**
+     * 更新一个元素
+     * @param index
+     * @param e
+     */
+    public void set(int index, E e){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("index out of bound!");
+
+        Node cur = dummyHead.next;
+        for(int i = 0; i < index; i ++){
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    /**
+     * 是否包含某个元素
+     * @param e
+     * @return
+     */
+    public boolean contains(E e){
+        Node cur = dummyHead.next;
+        while (cur != null){
+            if(cur.e.equals(e))
+                return true;
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            str.append(cur.e + "->");
+            cur = cur.next;
+        }
+        str.append("NULL");
+
+        return str.toString();
     }
 }
--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for(int i = 0; i < 10; i ++){
+            linkedList.addFirst(i);
+            System.out.println(linkedList.toString());
+        }
+
+        linkedList.add(6, 80);
+        System.out.println(linkedList.toString());
     }
 }
