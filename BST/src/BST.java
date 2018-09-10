e3b88f3299b13bf4f61f9d148ca807f75080ea54
border <border.gong@gmail.com>
2018-09-10 21:23:36 +0800

    更新插入节点算法

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -21,30 +21,21 @@ public class BST<E extends Comparable<E>> {
         }
 
         public void add(E e){
-            if(root == null)
-                root = new Node(e);
-            else
-                add(root, e);
-
+            root = add(root, e);
         }
 
-        private void add(Node node, E e){
-            if(e.equals(node.e))
-                return;
-            else if(e.compareTo(node.e) < 0 && node.left == null) {
-                node.left = new Node(e);
-                size++;
-            }
-            else if(e.compareTo(node.e) > 0 && node.right == null) {
-                node.right = new Node(e);
-                size++;
+        private Node add(Node node, E e){
+            if(node == null){
+                size ++;
+                return new Node(e);
             }
             else if(e.compareTo(node.e) < 0){
-                add(node.left, e);
-            }else{
-                add(node.right, e);
+                node.left = add(node.left, e);
+            }else if(e.compareTo(node.e) > 0){
+                node.right = add(node.right, e);
             }
 
+            return node;
         }
     }
 }
